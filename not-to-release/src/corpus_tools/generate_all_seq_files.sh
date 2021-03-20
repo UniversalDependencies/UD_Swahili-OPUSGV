80de0ab073bd137bf4ac3a3c0a451c2a62960e77
ksteimel <ksteimel@iu.edu>
2021-03-20 16:04:00 -0400

    Adding scripts for generating training files

---
+++ not-to-release/src/corpus_tools/generate_all_seq_files.sh
@@ -0,0 +1,5 @@
+files=`ls data/hcs-na-v2/`
+for file in $files
+do
+	python3 generate_seq_tag_file.py --in_json data/hcs-na-v2/$file --out_file data/allennlp-helsinki-morph/$file --label 'morph' > $file-unlabeled-log.txt
+done
